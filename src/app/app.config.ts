6b5652c39a3f439d1c8c6c0449f23796776a8966
mahmoudali-ops <mahmoua487@gmail.com>
2026-02-06 02:00:43 +0200

    asd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,10 +15,11 @@ export const appConfig: ApplicationConfig = {
       withInMemoryScrolling({scrollPositionRestoration: 'top'}),
       withViewTransitions()
     ), 
+    provideClientHydration(),
     provideHttpClient(
-      // تم إزالة withFetch()  لأنها تسبب مشاكل مع Cookies في Cross-site scenarios
+      // تم إزالة withFetch() لأنها تسبب مشاكل مع Cookies في Cross-site scenarios
       // Fetch API لا يرسل Cookies بشكل موثوق مع SameSite=None و Secure=true
-      withInterceptors([authInterceptorInterceptor, langInterceptor])
+      withInterceptors([langInterceptor,authInterceptorInterceptor])
     ),
     provideToastr(),
      provideAnimations()
